90b5f9dd7a7b06606353ebe79520be4d908bb270
samana0104 <b_hb0104@naver.com>
2026-07-29 11:49:06 +0900

    main에 에러 발생시 추적 가능한 traceback 추가

--- src/main.py
+++ src/main.py
@@ -1,14 +1,15 @@
 import System
+import traceback
 
 if __name__ == "__main__":
-    System.FunctionLibrary.log("Starting system...", System.LogLevel.DANGER)
+    System.FunctionLibrary.log("Starting system...", System.LogLevel.NONE)
     application = System.Application()
 
     try:
         System.FunctionLibrary.log("System is running. Press Ctrl+C to stop.", System.LogLevel.NONE)
         application.run()
     except Exception as e:
-        System.FunctionLibrary.log(f"An error occurred: {e}", System.LogLevel.DANGER)
+        System.FunctionLibrary.log(traceback.format_exc(), System.LogLevel.DANGER)
     except KeyboardInterrupt:
         print("\nStopping system...")
     finally:
