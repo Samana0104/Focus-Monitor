f182bef541bead840c2494970bb1bc7128bc409e
samana0104 <b_hb0104@naver.com>
2026-07-29 12:13:16 +0900

    스타일 별로라서 한줄로 바꿈

--- src/Main.py
+++ src/Main.py
@@ -8,18 +8,12 @@ if __name__ == "__main__":
     application = System.Application()
 
     try:
-        System.FunctionLibrary.log(
-            "System is running. Press Ctrl+C to stop.",
-            System.LogLevel.NONE,
-        )
+        System.FunctionLibrary.log("System is running. Press Ctrl+C to stop.", System.LogLevel.NONE)
         application.run()
     except KeyboardInterrupt:
         print("\nStopping system...")
     except Exception:
-        System.FunctionLibrary.log(
-            traceback.format_exc(),
-            System.LogLevel.DANGER,
-        )
+        System.FunctionLibrary.log( traceback.format_exc(), System.LogLevel.DANGER)
     finally:
         System.FunctionLibrary.log("System has stopped.", System.LogLevel.NONE)
         application.stop()
